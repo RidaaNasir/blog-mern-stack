2a68535e6060365f803f422f83717b23ea814a53
Ridaa Nasir <ridaanasirr@outlook.com>
2024-04-20 02:48:46 +0300

    added images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className='nav__logo'>
+          <img src='' alt=''/>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
