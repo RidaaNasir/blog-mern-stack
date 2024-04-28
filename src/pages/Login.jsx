1e4a7f00ac8160c9e6af1c56669d2a11767f2dd2
Ridaa Nasir <ridaanasirr@outlook.com>
2024-04-28 04:55:09 +0300

    updated profile page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const changeInputHandler = (e) => {
         <h2>sign in</h2>
         <form className="form login__form">
           <p className="form__error-message">This is an error message</p>
-          <input type="text" placeholder='Email' name='email'  value={userData.email} onChange={changeInputHandler}/>
+          <input type="text" placeholder='Email' name='email'  value={userData.email} onChange={changeInputHandler} autoFocus/>
           <input type="password" placeholder='Password' name='password'  value={userData.password} onChange={changeInputHandler}/>
           <button type='submit' className='btn primary'>login</button>
           <small>Don't  have an account? <Link to="/register"> Sign up</Link></small>
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import '../css/profile.css'
+import '../css/register.css'
 
 import Avatar from '../images/mern-blog-assets-main/avatar15.jpg'
 import { FaEdit } from "react-icons/fa";
@@ -8,7 +9,12 @@ import { FaCheck } from "react-icons/fa";
 
 const UserProfile = () => {
 
-  const[avatar,setAvatar] = useState('')
+  const[avatar,setAvatar] = useState(Avatar)
+  const[name,setName] = useState('')
+  const[email,setEmail] = useState('')
+  const[currentPassowrd,setCurrentPassword] = useState('')
+  const[newPassword,setNewPassword] = useState('')
+  const[confirmNewPassword,setConfirmNewPassword] = useState('')
 
 
 
@@ -20,7 +26,7 @@ const UserProfile = () => {
         <div className="profile__detail">
           <div className="avatar__wrapper">
             <div className="profile__avatar">
-              <img src={Avatar} alt="" />
+              <img src={avatar} alt="" />
             </div>
             {/*form to update avatar/image */}
             <form className="avatar__form">
@@ -32,6 +38,19 @@ const UserProfile = () => {
           </div>
 
           <h1>Ridaa Nasir</h1>
+
+          {/* form to update users details */}
+
+          <form  className="form profile__form">
+            <p className="form__error-message">This is an error message</p>
+            <input type='text' placeholder='Full Name' value={name} onChange={e => setName(e.target.value)}/>
+            <input type='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
+            <input type='password' placeholder='Current Password' value={currentPassowrd} onChange={e => setCurrentPassword(e.target.value)}/>
+            <input type='password' placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
+            <input type='password' placeholder='Confirm Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)}/>
+
+            <button type='submit' className='btn primary'>Update Details</button>
+          </form>
         </div>
       </div>
     </section>
