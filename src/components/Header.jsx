dc4ce8b9e8ff49841875de4b6c85366d989bbc81
Ridaa Nasir <ridaanasirr@outlook.com>
2024-05-01 01:16:26 +0300

    setting nodejs and Mongo db for the website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+
+const closeNavHandler = () => {
+  if(window.innerWidth < 800){
+    setIsNavShowing(false)
+  }else
+  setIsNavShowing(true)
+
+}
+
   return (
     <nav>
       <div className="container nav__container">
-        <Link to="/" className='nav__logo'>
+        <Link to="/" className='nav__logo' onClick={closeNavHandler}>
           <img src='' alt=''/>
         </Link>
-        <ul className="nav__menu">
-          <li><Link to="/profile/sdfsdf">Ridaa Nasir</Link></li>
-          <li><Link to="/create">created post</Link></li>
-          <li><Link to="/authors">Authors</Link></li>
-          <li><Link to="/logout">logout</Link></li>
-        </ul>
-        <button className="nav_toggle-btn">
-        <AiOutlineClose />
+        {isNavShowing && <ul className="nav__menu">
+          <li><Link to="/profile/sdfsdf" onClick={closeNavHandler}>Ridaa Nasir</Link></li>
+          <li><Link to="/create" onClick={closeNavHandler}>created post</Link></li>
+          <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
+          <li><Link to="/logout" onClick={closeNavHandler}>logout</Link></li>
+        </ul>}
+        <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+        {isNavShowing ? <AiOutlineClose /> : <FaBars />}
 
         </button>
       </div>
